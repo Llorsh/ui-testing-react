4f0f0068f7b1dfb80de2fdb355126704baa546fd
Jorge Arias <jorge.a@estudiomoca.cl>
2025-05-22 18:36:10 -0400

    chore: first version of UI library

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -5,7 +5,7 @@ import Button from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Atoms/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -15,7 +15,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    // backgroundColor: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -25,29 +25,44 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Primario: Story = {
   args: {
-    primary: true,
+    buttonType: "primary",
     label: 'Button',
+    type: 'button',
   },
 };
 
-export const Secondary: Story = {
+export const Primario_Medicos: Story = {
   args: {
+    buttonType: "primaryDoctor",
+    size: 'base',
     label: 'Button',
+    type: 'button',
   },
 };
 
-export const Large: Story = {
+export const Secundario: Story = {
   args: {
-    size: 'large',
+    buttonType: "secondary",
     label: 'Button',
+    type: 'button',
   },
 };
 
-export const Small: Story = {
+export const Outline: Story = {
   args: {
-    size: 'small',
+    buttonType: "outline",
     label: 'Button',
+    type: 'button',
   },
 };
+
+export const Texto: Story = {
+  args: {
+    buttonType: "text",
+    label: 'Button',
+    type: 'button',
+  },
+};
+
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,40 +1,65 @@
-import React from 'react';
-
-import './button.css';
 
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
   /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
+  size?: "xs" | "sm" | "base" | "lg" | "xl";
   /** Button contents */
   label: string;
   /** Optional click handler */
   onClick?: () => void;
+  /** Optional type */
+  type?: "button" | "submit" | "reset";
+  /** Optional disabled */
+  disabled?: boolean;
+  buttonType?: "primary" | "primaryDoctor" | "secondary" | "outline" | "text";
 }
+
+
 
 /** Primary UI component for user interaction */
 const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
+  size = "base",
+  type = "button",
   label,
+  disabled = false,
+  buttonType = "primary",
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+
+  const lightMode =
+    buttonType === "primary"
+      ? "bg-primary text-white hover:bg-primary-hover hover:text-white disabled:bg-disabled font-bold"
+      : buttonType === "primaryDoctor"
+        ? "bg-primary-doctor text-white hover:bg-primary-doctor-hover hover:text-white disabled:bg-disabled font-bold"
+        : buttonType === "secondary"
+          ? "bg-secondary text-primary hover:bg-secondary-hover hover:text-primary disabled:bg-disabled font-bold disabled:text-white"
+          : buttonType === "outline"
+            ? "outline outline-primary text-primary hover:bg-primary hover:text-white disabled:bg-white font-bold disabled:text-disabled disabled:outline-disabled"
+            : buttonType === "text"
+              ? "font-medium text-primary hover:font-bold disabled:text-disabled disabled:font-medium"
+              : "bg-secondary text-primary hover:bg-secondary-hover hover:text-primary disabled:bg-disabled font-bold disabled:text-white";
+
+  const sizeClasses = {
+    xs: "text-xs px-4 py-2",
+    sm: "text-sm px-4 py-3",
+    base: "text-base px-6 py-4",
+    lg: "text-lg px-8 py-5",
+    xl: "text-xl px-10 py-6 ",
+  };
+
   return (
     <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      type={type}
+      disabled={disabled}
+      className={[
+        "rounded-lg",
+        sizeClasses[size],
+        lightMode
+      ].join(" ")}
       {...props}
     >
       {label}
     </button>
   );
 };
-
 export default Button;
-
